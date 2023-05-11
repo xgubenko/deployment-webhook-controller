42ab8798366029ad36432cbf53926f41a6a08498
Aleksandr Gubenko <asd@Aleksandrs-MacBook-Pro.local>
2023-05-11 14:39:52 +0300

    moved to maven

--- src/main/kotlin/tech/gubenko/Main.kt
+++ src/main/kotlin/tech/gubenko/Main.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Main
+open class Main
 
 fun main(args: Array<String>) {
     runApplication<Main>(*args)
