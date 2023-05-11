283e596ca2502e4bc5b7014b1dcf1b9c4f9ebfd5
Aleksandr Gubenko <asd@Aleksandrs-MacBook-Pro.local>
2023-05-11 15:32:37 +0300

    small fixes

--- src/main/kotlin/tech/gubenko/controller/DeploymentWebhookController.kt
+++ src/main/kotlin/tech/gubenko/controller/DeploymentWebhookController.kt
@@ -1,6 +1,5 @@
 package tech.gubenko.controller
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
@@ -16,23 +15,23 @@ class DeploymentWebhookController {
     @Value("\${script.path}")
     lateinit var scriptPath: String;
 
-    @Value("\${secret}")
-    lateinit var secret: String
+//    @Value("\${secret}")
+//    lateinit var secret: String
 
     @PostMapping("/deploy")
     fun test(@RequestBody json: String): ResponseEntity<String> {
         logger.info("Deploy started")
-        val mapper = jacksonObjectMapper()
-        val temp = mapper.readTree(json)
-        val requestSecret = temp.at("/hook/config/secret").textValue()
-        if (secret.equals(requestSecret)) {
-            val pb = ProcessBuilder(scriptPath)
-            pb.start()
-            logger.info("Deploy successful")
-            return ResponseEntity.status(HttpStatus.OK).build()
-        } else {
-            logger.warn("Deploy failed: secret incorrect")
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build()
-        }
+//        val mapper = jacksonObjectMapper()
+//        val temp = mapper.readTree(json)
+//        val requestSecret = temp.at("/hook/config/secret").textValue()
+//        if (secret.equals(requestSecret)) {
+        val pb = ProcessBuilder(scriptPath)
+        pb.start()
+        logger.info("Deploy successful")
+        return ResponseEntity.status(HttpStatus.OK).build()
+//        } else {
+//            logger.warn("Deploy failed: secret incorrect")
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build()
+
     }
 }
