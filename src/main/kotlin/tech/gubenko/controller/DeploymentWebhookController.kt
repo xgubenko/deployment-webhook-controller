3f3a01153f2a7b84789c77dc70b7a6f9c104a4e1
Aleksandr Gubenko <asd@Aleksandrs-MacBook-Pro.local>
2023-05-16 20:54:31 +0300

    verification fixes

--- src/main/kotlin/tech/gubenko/controller/DeploymentWebhookController.kt
+++ src/main/kotlin/tech/gubenko/controller/DeploymentWebhookController.kt
@@ -1,5 +1,6 @@
 package tech.gubenko.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.commons.codec.digest.HmacUtils
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
@@ -17,16 +18,13 @@ class DeploymentWebhookController {
     private val logger = LoggerFactory.getLogger(javaClass)
 
     @Value("\${script.path}")
-    lateinit var scriptPath: String;
+    lateinit var scriptPath: String
 
     @Value("\${repository.secret}")
     lateinit var repositorySecret: String
 
     @Value("\${repository.path}")
     lateinit var repositoryPath: String
-
-    @Value("\${repository.branch}")
-    lateinit var repositoryBranch: String
 
     @Value("\${repository.users}")
     lateinit var repositoryUsers: List<String>
@@ -43,20 +41,31 @@ class DeploymentWebhookController {
     @PostMapping("/deploy")
     fun test(@RequestHeader() headers: HttpHeaders, @RequestBody payload: String): ResponseEntity<Response> {
         val computed = String.format("%s=%s", encodingAlgorithm, HmacUtils.hmacSha1Hex(repositorySecret, payload))
-        val user = "test"
+
+        val root = ObjectMapper().readTree(payload)
+        val payloadUser = root.path("pusher").path("name").textValue()
+        val payloadRepository = root.path("repository").path("html_url").textValue()
+
         if (!headers.get(signatureHeader)!!.equals(computed)) {
             logger.info("Signature verification failed")
             return ResponseEntity<Response>(Response("Signature verification failed"), HttpStatus.UNAUTHORIZED)
         }
+        if (!repositoryUsers.contains(payloadUser)) {
+            logger.warn("User unauthorized: {}", payloadUser)
+            return ResponseEntity<Response>(Response("User unauthorized"), HttpStatus.UNAUTHORIZED)
+        }
 
-        if (!headers.get(eventHeader)!!.equals("push")) {
-            logger.info("ping successful")
+        if (!headers.get(eventHeader)!!.get(0).equals("push")) {
+            logger.info("Ping successful")
             return ResponseEntity<Response>(Response("Ping successful"), HttpStatus.OK)
         }
 
-        if (!repositoryUsers.contains(user)) {
-            logger.warn("User unauthorized")
-            return ResponseEntity<Response>(Response("User unauthorized"), HttpStatus.UNAUTHORIZED)
+        if (!repositoryPath.equals(payloadRepository)) {
+            logger.info("Repository with this origin will not be deployed: {}", payloadRepository)
+            return ResponseEntity<Response>(
+                Response("Repository with this origin will not be deployed"),
+                HttpStatus.BAD_REQUEST
+            )
         }
 
         logger.info("Deploy started")
