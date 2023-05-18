fde785b59ff5c46cc208241ad25e0e6717a77f77
Aleksandr Gubenko <asd@Aleksandrs-MacBook-Pro.local>
2023-05-18 22:32:35 +0300

    test

--- src/main/kotlin/tech/gubenko/controller/DeploymentWebhookController.kt
+++ src/main/kotlin/tech/gubenko/controller/DeploymentWebhookController.kt
@@ -17,38 +17,40 @@ class DeploymentWebhookController(val properties: WebhookProperties, val deployS
     private val logger = LoggerFactory.getLogger(javaClass)
 
     @PostMapping("/deploy")
-    fun test(@RequestHeader() headers: HttpHeaders, @RequestBody payload: String): ResponseEntity<Response> {
+    fun test(@RequestHeader() headers: HttpHeaders, @RequestBody payload: String): ResponseEntity<String> {
 
 
-        val payloadUser = deployService.getPusherName(payload)
-        val payloadRepository = deployService.getRepositoryUrl(payload)
-        val computed = deployService.computeSignature(payload)
+//        val payloadUser = deployService.getPusherName(payload)
+//        val payloadRepository = deployService.getRepositoryUrl(payload)
+//        val computed = deployService.computeSignature(payload)
+//
+//        if (!headers.get(properties.signatureHeader)!!.get(0).equals(computed)) {
+//            logger.info("Signature verification failed")
+//            return ResponseEntity<Response>(Response(String.format("Signature verification failed:%s, payload:%s, expected:%s", computed, payload, headers.get(properties.signatureHeader)!!)), HttpStatus.UNAUTHORIZED)
+//        }
+//        if (!properties.repositoryUsers.contains(payloadUser)) {
+//            logger.warn("User unauthorized: {}", payloadUser)
+//            return ResponseEntity<Response>(Response("User unauthorized"), HttpStatus.UNAUTHORIZED)
+//        }
+//
+//        if (!headers.get(properties.eventHeader)!!.get(0).equals("push")) {
+//            logger.info("Ping successful")
+//            return ResponseEntity<Response>(Response("Ping successful"), HttpStatus.OK)
+//        }
+//
+//        if (!properties.repositoryPath.equals(payloadRepository)) {
+//            logger.info("Repository with this origin will not be deployed: {}", payloadRepository)
+//            return ResponseEntity<Response>(
+//                Response("Repository with this origin will not be deployed"),
+//                HttpStatus.BAD_REQUEST
+//            )
+//        }
+//
+//        deployService.runDeploy()
+//
+//        return ResponseEntity<Response>(Response(String.format("Payload:%s, signature:%s", payload
+//        ,headers.get(properties.signatureHeader))), HttpStatus.OK)
 
-        if (!headers.get(properties.signatureHeader)!!.get(0).equals(computed)) {
-            logger.info("Signature verification failed")
-            return ResponseEntity<Response>(Response(String.format("Signature verification failed:%s, payload:%s, expected:%s", computed, payload, headers.get(properties.signatureHeader)!!)), HttpStatus.UNAUTHORIZED)
-        }
-        if (!properties.repositoryUsers.contains(payloadUser)) {
-            logger.warn("User unauthorized: {}", payloadUser)
-            return ResponseEntity<Response>(Response("User unauthorized"), HttpStatus.UNAUTHORIZED)
-        }
-
-        if (!headers.get(properties.eventHeader)!!.get(0).equals("push")) {
-            logger.info("Ping successful")
-            return ResponseEntity<Response>(Response("Ping successful"), HttpStatus.OK)
-        }
-
-        if (!properties.repositoryPath.equals(payloadRepository)) {
-            logger.info("Repository with this origin will not be deployed: {}", payloadRepository)
-            return ResponseEntity<Response>(
-                Response("Repository with this origin will not be deployed"),
-                HttpStatus.BAD_REQUEST
-            )
-        }
-
-        deployService.runDeploy()
-
-        return ResponseEntity<Response>(Response(String.format("Payload:%s, signature:%s", payload
-        ,headers.get(properties.signatureHeader))), HttpStatus.OK)
+        return ResponseEntity<String>(payload, HttpStatus.OK)
     }
 }
